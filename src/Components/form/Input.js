2588f0fc0fb671f2f4756828fa1fc63173ac264f
unknown <caio2002araujo@gmail.com>
2021-11-06 14:40:02 -0300

    adding error component

--- src/Components/form/Input.js
+++ src/Components/form/Input.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './Input.module.css';
+import Error from '../../Helper/Error';
 
-const Input = ({label, id, type, value, onChange, onBlur}) => {
+const Input = ({label, id, type, value, error, onChange, onBlur}) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={id} className={styles.label}>{label}</label>
       <input type={type} id={id} name={id} className={styles.input} value={value} onChange={onChange} onBlur={onBlur}/>
+
+      {error && <Error error={error} />}
     </div>
   )
 }
