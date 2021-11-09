69612e8c1d8f9daf4fa7ef8bc827938ad3f78870
unknown <caio2002araujo@gmail.com>
2021-11-09 19:15:55 -0300

    adding error handling to authentication

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -6,19 +6,31 @@ export const AuthContext = React.createContext();
 
 export const AuthProvider = ({children}) => {
   const [login, setLogin] = React.useState(null);
-  // const [loading, setLoading] = React.useState(null);
-  // const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
   const signIn = async (username, password) => {
-    const {url, options} = TOKEN_POST({usuario: username, hash: password});
-    const response = await fetch(url, options);
-    const {Token} = await response.json();
-    window.localStorage.setItem('token', Token);
-    setLogin(true);
+    try{
+      setError(null);
+      setLoading(true);
+      const {url, options} = TOKEN_POST({usuario: username, hash: password});
+      const response = await fetch(url, options);
+      if(!response.ok) throw new Error('Erro: credenciais inválidas');
+      const {Token} = await response.json();
+      window.localStorage.setItem('token', Token);
+      setLogin(true);
+    }
+    catch(err){
+      setError(err.message);
+      setLogin(false);
+    }
+    finally{
+      setLoading(false);
+    }
   }
 
   return (
-    <AuthContext.Provider value={{signIn, login}}>
+    <AuthContext.Provider value={{signIn, login, loading, error}}>
       {children}
     </AuthContext.Provider>
   )
