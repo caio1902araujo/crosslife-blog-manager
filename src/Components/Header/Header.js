4749e7b8ed7e18face29ad475f992aa599c8c59b
unknown <caio2002araujo@gmail.com>
2021-11-18 20:53:58 -0300

    adding hook click out

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Hooks/useAuth';
 import Logo from '../Logo/Logo';
+import Submenu from '../Submenu/Submenu';
 import {ReactComponent as  Magnifier} from '../../Assets/magnifier.svg';
 import {ReactComponent as Sun} from '../../Assets/sun.svg';
 import {ReactComponent as User} from '../../Assets/user.svg';
-import { AuthContext } from '../../Hooks/useAuth';
 
 const Header = () => {
+  const {signOut} = React.useContext(AuthContext);
   const [active, setActive] = React.useState(false);
   
-  const {signOut} = React.useContext(AuthContext);
-  
+
   return (
     <header className={styles.header}>
       <div className={`container ${styles.wrapper}`}>
@@ -19,13 +20,17 @@ const Header = () => {
 
         <Link to="pesquisa" className={styles.itemHeader}><Magnifier/></Link>
         <div className={styles.itemHeader}><Sun/></div>
-        <div className={styles.itemHeader} onClick={() => setActive(!active)} aria-controls="submenuUser"><User/></div>
-
-        <nav id="submenuUser" className={`${active ? "animeRight" : ""} ${styles.nav}`}>
-          <Link to=''>Suas noticias</Link>
-          <Link to='estatisticas'>Estatisticas</Link>
-          <button onClick={signOut}>Sair</button>
-        </nav>
+        <div className={styles.itemHeader} onClick={() => {setActive(!active)}} aria-controls="submenuUser"><User/></div>
+        
+        {
+          active && 
+          <Submenu id="submenuUser" setActive={setActive}>
+            <Link to=''>Suas noticias</Link>
+            <Link to='estatisticas'>Estatisticas</Link>
+            <button onClick={signOut}>Sair</button>
+          </Submenu>
+        }
+        
       </div>
     </header>
   )
