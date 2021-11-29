e21aa1a77ac4002af9a6838ab761edfb117b6d99
unknown <caio2002araujo@gmail.com>
2021-11-29 15:04:22 -0300

    adding component to article

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import styles from './Article.module.css'
 
-const Article = () => {
+const Article = ({key, news}) => {
+  const convertDate = (dateAmericanFormat, format) => {
+    const date = new Date(dateAmericanFormat);
+    return date.toLocaleDateString(format)
+  }
+
+  console.log(key);
   return (
     <article className={styles.article}>
-      <h3 className={styles.aticleTitle}>Afinal whey ou chimarrão</h3>
-      <p className={styles.aticleText}>por <span className={styles.name}>Yudi Yoshimine</span> há 4 horas</p>
-      <span className={styles.category}>saúde</span>
+      <h3 className={styles.aticleTitle}>{news.titulo}</h3>
+      <p className={styles.aticleText}>postado em <span className={styles.date}>{convertDate(news.data_criacao, 'pt-BR')}</span></p>
+      <span className={styles.category}>{news.categoria}</span>
     </article>
-  )
+  );
 }
 
 export default Article
