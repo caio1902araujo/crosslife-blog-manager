9caa1d6330284ac3d6740191551693dd6a0bd870
unknown <caio2002araujo@gmail.com>
2021-12-01 23:37:14 -0300

    adding state and modal component to delete news

--- src/Pages/News/NewsFeed.js
+++ src/Pages/News/NewsFeed.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Feed from '../../Components/Feed/Feed';
 import HeaderNews from '../../Components/HeaderNews/HeaderNews';
+import ModalDelete from '../../Components/ModalDelete/ModalDelete';
 
 const NewsFeed = () => {
+  const [modalDelete, setModalDelete] = React.useState(false);
+
   return (
     <section>
       <HeaderNews title="Suas Noticias" buttonSecondary="Criar Notícia"/>
-      <Feed/>
+      <Feed setModalDelete={setModalDelete}/>
+      {
+        modalDelete && <ModalDelete setModalDelete={setModalDelete} modalDelete={modalDelete}/>
+      }
     </section>
   )
 }
 
-export default NewsFeed
+export default NewsFeed;
