ec83ab7ad5175cb613780b13b4de71429eb41650
unknown <caio2002araujo@gmail.com>
2021-12-04 01:16:32 -0300

    removing section tag and adding object to configure secondary button

--- src/Pages/News/NewsFeed.js
+++ src/Pages/News/NewsFeed.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import Feed from '../../Components/Feed/Feed';
 import HeaderNews from '../../Components/HeaderNews/HeaderNews';
 import ModalDelete from '../../Components/ModalDelete/ModalDelete';
 
 const NewsFeed = () => {
   const [modalDelete, setModalDelete] = React.useState(false);
+  const navigate = useNavigate();
+  const buttonSecondaryConfig = {
+    content: "Criar Notícia",
+    onClick: () => {navigate("criar")}
+  }
 
   return (
-    <section>
-      <HeaderNews title="Suas Noticias" buttonSecondary="Criar Notícia"/>
+    <>
+      <HeaderNews title="Suas Notícias" buttonSecondary={buttonSecondaryConfig}/>
       <Feed setModalDelete={setModalDelete}/>
       {
         modalDelete && <ModalDelete setModalDelete={setModalDelete} modalDelete={modalDelete}/>
       }
-    </section>
+    </>
   )
 }
 
