83568a2118f35fef6c05856eb71f423f1fa5f583
caio1902araujo <caio2002araujo@gmail.com>
2021-12-28 16:56:09 -0300

    creating edit news screen

--- src/Pages/News/NewsEdit.js
+++ src/Pages/News/NewsEdit.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import useFetch from '../../Hooks/useFetch'
+import { useParams } from 'react-router-dom'
+import { NEWS_GET_ID } from '../../Services/API'
+import FormCreateNews from '../../Components/FormCreateNews/FormCreateNews'
 
 const NewsEdit = () => {
-  return (
-    <div>
-      editar noticia
-    </div>
-  )
+  const {id} = useParams();
+  const {data, request} = useFetch();
+
+  React.useEffect(()=>{
+    const requestNews = async () => {
+      const {url, options} = NEWS_GET_ID(id);
+      await request(url, options);
+    } 
+    requestNews();
+  }, [request, id]);
+
+  if(data){
+    return <FormCreateNews methodForm="put" dateForm={data}/>;
+  }
+  else return null;
 }
 
 export default NewsEdit
