a87e2d5f899753870923841194f0ee243070d9c2
caio1902araujo <caio2002araujo@gmail.com>
2021-12-28 15:42:21 -0300

    creating news creation screen

--- src/Pages/News/NewsCreate.js
+++ src/Pages/News/NewsCreate.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import FormCreateNews from '../../Components/FormCreateNews/FormCreateNews'
 
 const NewsCreate = () => {
   return (
-    <div>
-      criar noticia
-    </div>
+    <FormCreateNews methodForm="post"/>
   )
 }
 
