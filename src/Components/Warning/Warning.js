6643426bfc0e940e89758e0856d1202eea9e3bac
caio1902araujo <caio2002araujo@gmail.com>
2022-01-05 19:57:02 -0300

    creating warning component for internal error

---
+++ src/Components/Warning/Warning.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Warning.module.css';
+import {ReactComponent as ErrorIcon} from '../../Assets/error.svg';
+
+const Warning = () => {
+  return (
+    <section className={styles.wrapperIcon}>
+      <div>
+        <ErrorIcon />
+      </div>
+      
+      <div className={styles.contentError}>
+        <h2 className='subtitle'>Erro ao carregar notícias</h2>
+        <p>Infelizmente tivemos problemas internos ao carregar as notícias, por favor tente novamente mais tarde</p>
+      </div>
+    </section>
+  )
+}
+
+export default Warning
