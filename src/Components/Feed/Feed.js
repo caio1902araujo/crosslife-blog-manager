fc615b240fd54712d1616534f67a87feec054054
caio1902araujo <caio2002araujo@gmail.com>
2022-01-06 23:52:00 -0300

    adding state in case there is a change in the news feed

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -4,14 +4,18 @@ import useFetch from '../../Hooks/useFetch'
 import { NEWS_GET } from '../../Services/API';
 import Article from '../Article/Article';
 import Warning from '../Warning/Warning';
-import PropTypes from 'prop-types';
+import ModalDelete from '../ModalDelete/ModalDelete';
+// import PropTypes from 'prop-types';
 
-const Feed = ({setModalDelete}) => {
+const Feed = () => {
   const {data, error, loading, request} = useFetch();
+  const [modalDelete, setModalDelete] = React.useState(false);
+  const [changeFeed, setChangeFeed] = React.useState(0)
+
   React.useEffect(() => {
     const {url, options} = NEWS_GET();
     request(url, options);
-  }, [request]);
+  }, [request, changeFeed]);
 
   if(error) return <Warning />
   if(loading) return <p>Carregando...</p>
@@ -21,13 +25,16 @@ const Feed = ({setModalDelete}) => {
         {
           data.map((news) => <Article key={news.id} news={news} setModalDelete={setModalDelete}/>)
         }
+        {
+          modalDelete && <ModalDelete setModalDelete={setModalDelete} newsData={modalDelete} setChangeFeed={setChangeFeed}/>
+        }
       </section>
     )
   else return null
 }
 
-Feed.propTypes = {
-  setModalDelete: PropTypes.func.isRequired,
-}
+// Feed.propTypes = {
+//   setModalDelete: PropTypes.func.isRequired,
+// }
 
 export default Feed
