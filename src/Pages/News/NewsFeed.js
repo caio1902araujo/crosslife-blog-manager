c3a9cf8b77decb36409b7b0ca5e7dda4b06c1d81
caio1902araujo <caio2002araujo@gmail.com>
2022-01-06 23:53:24 -0300

    removing modal to delete news

--- src/Pages/News/NewsFeed.js
+++ src/Pages/News/NewsFeed.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 import Feed from '../../Components/Feed/Feed';
 import HeaderNews from '../../Components/HeaderNews/HeaderNews';
-import ModalDelete from '../../Components/ModalDelete/ModalDelete';
 
 const NewsFeed = () => {
-  const [modalDelete, setModalDelete] = React.useState(false);
   const navigate = useNavigate();
   const buttonSecondaryConfig = {
     content: "Criar Notícia",
@@ -15,10 +13,7 @@ const NewsFeed = () => {
   return (
     <>
       <HeaderNews title="Suas Notícias" buttonSecondary={buttonSecondaryConfig}/>
-      <Feed setModalDelete={setModalDelete}/>
-      {
-        modalDelete && <ModalDelete setModalDelete={setModalDelete} newsData={modalDelete}/>
-      }
+      <Feed/>
     </>
   )
 }
