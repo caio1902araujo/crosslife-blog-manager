7a0aafbdc03ac207c6f4aa6b0eb1059197ec3e20
caio1902araujo <caio2002araujo@gmail.com>
2022-01-09 20:49:17 -0300

    adding modal component to alerts

--- src/Pages/News/NewsFeed.js
+++ src/Pages/News/NewsFeed.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
+import { AuthContext } from '../../Hooks/useAuth';
+import Alert from '../../Components/Alert/Alert';
 import Feed from '../../Components/Feed/Feed';
 import HeaderNews from '../../Components/HeaderNews/HeaderNews';
 
 const NewsFeed = () => {
   const navigate = useNavigate();
+  const {alert} = React.useContext(AuthContext);
   const buttonSecondaryConfig = {
     content: "Criar Notícia",
     onClick: () => {navigate("criar")}
@@ -14,6 +17,7 @@ const NewsFeed = () => {
     <>
       <HeaderNews title="Suas Notícias" buttonSecondary={buttonSecondaryConfig}/>
       <Feed/>
+      {alert && <Alert message={alert.message} typeAlert={alert.typeAlert}/> }
     </>
   )
 }
