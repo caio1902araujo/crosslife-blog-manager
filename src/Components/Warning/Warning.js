1492db431e00a185202ede7f2b0b1740c00175fd
caio1902araujo <caio2002araujo@gmail.com>
2022-01-12 15:11:34 -0300

    adding title and description property to the warning component

--- src/Components/Warning/Warning.js
+++ src/Components/Warning/Warning.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './Warning.module.css';
 import {ReactComponent as ErrorIcon} from '../../Assets/error.svg';
+import PropTypes from 'prop-types';
 
-const Warning = () => {
+const Warning = ({title, description}) => {
   return (
     <section className={styles.wrapperWarning}>
       <div>
@@ -10,11 +11,16 @@ const Warning = () => {
       </div>
       
       <div className={styles.contentError}>
-        <h2 className='subtitle'>Erro ao carregar notícias</h2>
-        <p>Infelizmente tivemos problemas internos ao carregar as notícias, por favor tente novamente mais tarde</p>
+        <h2 className='subtitle'>{title}</h2>
+        <p>{description}</p>
       </div>
     </section>
   )
 }
 
-export default Warning
+Warning.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
+
+export default Warning;
