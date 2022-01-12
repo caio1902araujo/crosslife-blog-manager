f0d52c0cd7b8b336d52f0943e6601e587af0a6e6
caio1902araujo <caio2002araujo@gmail.com>
2022-01-11 23:43:53 -0300

    typing parameter loader component description

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Loader.module.css';
+import PropTypes from 'prop-types';
 
-const Loader = () => {
+const Loader = ({description}) => {
   return (
     <div className={styles.wrapperLoader}>
       <div className={styles.loader}></div>
-      <p className={styles.description}>Carregando noticias</p>
+      <p className={styles.description}>{description}</p>
     </div>
   )
 }
+
+Loader.propTypes = {
+  description: PropTypes.string.isRequired,
+}
+
 
 export default Loader
