66f3ea7f5a210c63c18dc971b9dbf25bfc9b924c
caio1902araujo <caio2002araujo@gmail.com>
2022-02-06 20:44:16 -0300

    fixed query string bug in filters

--- src/Components/Filters/Filters.js
+++ src/Components/Filters/Filters.js
@@ -4,8 +4,15 @@ import useSelect from '../../Hooks/useSelect';
 import useCategory from '../../Hooks/useCategory';
 import Select from '../Select/Select';
 import CategoryList from '../CategoryList/CategoryList';
+import PropTypes from 'prop-types';
 
 const listItems = ["todos", "academia", "esportes", "fitness", "nutrição", "receitas", "saúde"];
+
+const defautValues = {
+  order: "asc",
+  category: "todos"
+};
+
 const options = [
   {
     text: "Mais recente",
@@ -17,48 +24,26 @@ const options = [
   },
 ];
 
-const defautValues = {
-  select: "asc",
-  category: "todos"
-}
-
-const configParams = [
-  {
-    key: "order",
-    value: defautValues.select,
-  },
-  {
-    key: "category",
-    value: defautValues.category,
-  }
-]
-
-const Filters = () => {
-  const selectDate = useSelect(defautValues.select, options);
+const Filters = ({setQueryString}) => {
+  const selectDate = useSelect(defautValues.order, options);
   const category = useCategory(defautValues.category, "todos");
   
   React.useEffect(()=>{
     const url = new URL(window.location);
     const params = url.searchParams;
-    const filterValues = {
+    const valuesParams = {
       order: selectDate.value,
       category: category.value,
     }
     
-    const param = configParams.find((param) => {
-      if (param.value !== filterValues[param.key]){
-        param.value = filterValues[param.key];
-        return param;
+    for(let param in valuesParams){
+      if(params.get(param) || valuesParams[param] !== defautValues[param]){
+        params.set(param, valuesParams[param]);
+        window.history.pushState({}, '', url);
+        setQueryString(params.toString());
       }
-      else return null;
-    });
-    
-    if(param){
-      params.set(param.key, param.value);
-      window.history.pushState({}, '', url);
     }
-    
-  },[selectDate, category]);
+  },[selectDate.value, category.value, setQueryString]);
 
   return (
     <div className={styles.filters}>
@@ -67,5 +52,9 @@ const Filters = () => {
     </div>
   );
 };
+
+Filters.propTypes = {
+  setQueryString: PropTypes.func,
+}
 
 export default Filters;
